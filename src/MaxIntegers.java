5b2660051ab131d74541eba2c17309bb90f04d3f
Aluno <Aluno@10.85.198.42>
2024-06-19 17:06:33 -0300

    java project MaxIntegers

---
+++ src/MaxIntegers.java
@@ -0,0 +1,16 @@
+public class MaxIntegers {
+public static void main(String[] args) {
+int num1 = 3, num2 = 3, num3 = 3;
+int max = 0;
+if (num1 > max) {
+max = num1;
+}//fim if
+if (num2 > max) {
+max = num2;
+}//fim if
+if (num3 > max) {
+max = num3;
+}//fim if
+System.out.println("O maximo de 3 numeros e " + max);
+}//fim do método main
+}
